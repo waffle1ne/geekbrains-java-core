6d2294bc31e7481837eb0f9344242a61bd849f4a
VTB50637337 <vemelyanov@vtb.ru>
2020-11-03 20:45:05 +0300

    lesson-10

---
+++ src/main/java/ru/geekbrains/homework10/Lesson10.java
@@ -0,0 +1,34 @@
+package ru.geekbrains.homework10;
+
+import okhttp3.OkHttpClient;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class Lesson10 {
+    public static void main(String[] args) {
+        //первое задание
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    new Server(8189);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Client();
+            }
+        });
+        //второе задание
+        try {
+            OkHttpRequest.Request("https://ya.ru");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/ru/geekbrains/homework10/Server.java
@@ -0,0 +1,39 @@
+package ru.geekbrains.homework10;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private final ServerSocket serverSocket;
+    private final Socket socket;
+    private final DataInputStream in;
+    private final DataOutputStream out;
+
+    public Server(int socketNum) throws IOException {
+        serverSocket = new ServerSocket(socketNum);
+        System.out.println("Сервер запущен...");
+        socket = serverSocket.accept();
+        System.out.println("Клиент подключился");
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+        while (true) {
+            String str = in.readUTF();
+            if (str.equals("/end")) {
+                out.writeUTF("Сервер отключён");
+                break;
+            }
+            out.writeUTF("Вы: " + str + "\n" + "Эхо: " + str);
+        }
+    }
+
+//    public static void main(String[] args) {
+//        try {
+//            Server server = new Server(8189);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
