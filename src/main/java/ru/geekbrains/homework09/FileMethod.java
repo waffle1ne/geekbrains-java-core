20668a356e0a58f4a153cccb5d29d6e664959e9f
waffle_one <54597478+waffle1ne@users.noreply.github.com>
2020-11-05 22:41:20 +0300

    lesson-09 (#10)
    * lesson-09
    
    * delUnusedImport
    
    * changeReadline_Read
    
    Co-authored-by: VTB50637337 <vemelyanov@vtb.ru>

---
+++ src/main/java/ru/geekbrains/homework09/FileMethod.java
@@ -0,0 +1,58 @@
+package ru.geekbrains.homework09;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileMethod {
+    public static int countSeq(Path filePath, String seq) throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(filePath, StandardCharsets.UTF_8)) {
+            StringBuilder str = new StringBuilder();
+            int count = 0;
+            int nextChar;
+            while ((nextChar = reader.read()) != -1) {
+                if (str.length() < seq.length()) {
+                    str.append((char) nextChar);
+                    continue;
+                }
+                if (str.toString().equals(seq))
+                    count++;
+                str.deleteCharAt(0);
+                str.append((char) nextChar);
+            }
+            return count;
+        }
+    }
+
+    public static void joinFilesText(Path dirPath, Path newFilePath) throws IOException {
+        Files.createFile(newFilePath);
+        try (BufferedWriter writer = Files.newBufferedWriter(newFilePath, StandardCharsets.UTF_8)) {
+            for (File file : dirPath.toFile().listFiles()) {
+                if (!file.isDirectory()) {
+                    try (BufferedReader reader = Files.newBufferedReader(file.toPath(), StandardCharsets.UTF_8)) {
+                        while (reader.ready())
+                            writer.write(reader.read());
+                    }
+                }
+            }
+            System.out.println("Сшивание завершено");
+        }
+    }
+
+    public static void rmRf(Path path) throws NullPointerException {
+        for (File file : path.toFile().listFiles()) {
+            if (file.isDirectory())
+                rmRf(file.toPath());
+            else {
+                System.out.println("Файл " + file.getName() + " удалён");
+                file.delete();
+            }
+        }
+        System.out.println("Папка " + String.valueOf(path.getFileName()) + " удалена");
+        path.toFile().delete();
+    }
+}
